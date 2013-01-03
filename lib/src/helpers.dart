31f8345a3175dca13acbac4d61c8ed574eb93f6d
Kevin Moore <kevin@thinkpixellab.com>
2013-01-03 12:55:53 -0500

    helpers: animation: make element argument required

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -15,38 +15,35 @@ part of animation;
  *
  * By default, linear interpolation is used, but you may specify another [easingType] method to use.
  */
-animate({
-  Element element,
+animate(Element element, {
   duration,
   EasingType easingType,
   Map<String, Object> properties,
   StepCallback onStep,
   bool paused
   }) {
-  var animation;
+  assert(element != null);
+  final animation = new StyleAnimation(element);
 
-  // Currently we just have StyleAnimation's, but in the future maybe others.
-  if (?element)
-    animation = new StyleAnimation(element);
-
-  if (animation is Animation) {
-    if (?properties)
-      animation.setProperties(properties);
-
-    if (?easingType)
-      animation.easingType = easingType;
-
-    if (?duration)
-      animation.duration = duration;
-
-    if (?onStep)
-      animation.onStep = onStep;
-
-    if (!?paused || paused == false)
-      animation.run();
-
-    return animation;
+  if (?properties) {
+    animation.setProperties(properties);
   }
 
-  throw new Exception('Could not perform the request animation. Did you specify an element to animate?');
+  if (?easingType) {
+    animation.easingType = easingType;
+  }
+
+  if (?duration) {
+    animation.duration = duration;
+  }
+
+  if (?onStep) {
+    animation.onStep = onStep;
+  }
+
+  if (!?paused || paused == false) {
+    animation.run();
+  }
+
+  return animation;
 }
