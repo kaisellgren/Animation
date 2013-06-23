da19b5bc3e1ab26b897f982250b81445f1f0d4b2
Kai Sellgren <kaisellgren@gmail.com>
2013-06-23 16:16:11 +0300

    Added tests and created a playground.

---
+++ test/playground.dart
@@ -0,0 +1,73 @@
+import 'dart:html';
+import 'dart:async';
+
+import 'package:animation/animation.dart';
+
+// This file is used to play around with the Animation lib to test things manually.
+
+main() {
+  var el = query('#box');
+
+  animate(el, duration: 1000, easing: Easing.LINEAR, properties: {
+    'left': 200
+  });
+
+  /*new AnimationQueue()
+  ..addAll([
+    animate(el, properties: {'opacity': 0.2}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 0.4}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 0.6}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 0.8}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 1.0}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 0.8}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 0.6}, duration: 1000, paused: true),
+    animate(el, properties: {'opacity': 0.4}, duration: 1000, paused: true),
+  ])
+  ..repeat = -1
+  ..interval = 1000
+  ..run();*/
+
+  /*var anim = new ElementAnimation(el)
+    ..duration = 5000
+    ..setProperties({
+      'left': 1000,
+      'top': 350
+    })
+    ..run();
+
+  new Timer(const Duration(milliseconds: 2500), () {
+    anim.pause();
+
+    new Timer(const Duration(milliseconds: 2500), () {
+      anim.run();
+    });
+  });*/
+
+/*var properties = {'left': 1000, 'top': 350};
+  var anim = animate(element: el, properties: properties, duration: 5000);
+
+  new Timer(2500, (Timer timer) {
+    anim.stop();
+
+    new Timer(2500, (Timer timer) {
+      anim.run();
+    });
+  });*/
+/*
+  var anim = new ElementAnimation(el)
+    ..duration = 1000
+    ..setProperties({
+      'left': 500,
+      'top': 250
+    });
+
+  var anim2 = new ElementAnimation(el)
+    ..duration = 500
+    ..setProperties({
+      'height': 250
+    });
+
+  var queue = new AnimationQueue()
+    ..addAll([anim, anim2])
+    ..run();*/
+}
--- test/test.dart
+++ test/test.dart
@@ -1,67 +1,123 @@
 import 'dart:html';
 import 'dart:isolate';
 
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_enhanced_config.dart';
+
 import 'package:animation/animation.dart';
 
 main() {
-  var el = query('#box');
+  useHtmlEnhancedConfiguration();
 
-  new AnimationQueue()
-    ..addAll([
-      animate(el, properties: {'opacity': 0.2}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 0.4}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 0.6}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 0.8}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 1.0}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 0.8}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 0.6}, duration: 1000, paused: true),
-      animate(el, properties: {'opacity': 0.4}, duration: 1000, paused: true),
-    ])
-    ..repeat = -1
-    ..interval = 1000
-    ..run();
+  group('Easing', () {
+    // Linear
 
-  /*var anim = new StyleAnimation(el)
-    ..duration = 5000
-    ..setProperties({
-      'left': 1000,
-      'top': 350
-    })
-    ..run();
+    test('Linear start', () => expect(EasingEngine.linear(0, 1000, 1000, 0), 0));
+    test('Linear middle', () => expect(EasingEngine.linear(100, 1000, 1000, 0), 100));
+    test('Linear end', () => expect(EasingEngine.linear(1000, 1000, 1000, 0), 1000));
 
-  new Timer(2500, (Timer timer) {
-    anim.pause();
+    // Quad
 
-    new Timer(2500, (Timer timer) {
-      anim.run();
-    });
-  });*/
+    test('Ease in quad start', () => expect(EasingEngine.easeInQuad(0, 1000, 1000, 0), 0));
+    test('Ease in quad middle', () => expect(EasingEngine.easeInQuad(500, 1000, 1000, 0), 250));
+    test('Ease in quad end', () => expect(EasingEngine.easeInQuad(1000, 1000, 1000, 0), 1000));
 
-  /*var properties = {'left': 1000, 'top': 350};
-  var anim = animate(element: el, properties: properties, duration: 5000);
+    test('Ease out quad start', () => expect(EasingEngine.easeOutQuad(0, 1000, 1000, 0), 0));
+    test('Ease out quad middle', () => expect(EasingEngine.easeOutQuad(500, 1000, 1000, 0), 750));
+    test('Ease out quad end', () => expect(EasingEngine.easeOutQuad(1000, 1000, 1000, 0), 1000));
 
-  new Timer(2500, (Timer timer) {
-    anim.stop();
+    test('Ease in/out quad start', () => expect(EasingEngine.easeInOutQuad(0, 1000, 1000, 0), 0));
+    test('Ease in/out quad middle', () => expect(EasingEngine.easeInOutQuad(500, 1000, 1000, 0), 500));
+    test('Ease in/out quad end', () => expect(EasingEngine.easeInOutQuad(1000, 1000, 1000, 0), 1000));
 
-    new Timer(2500, (Timer timer) {
-      anim.run();
-    });
-  });*/
-/*
-  var anim = new StyleAnimation(el)
-    ..duration = 1000
-    ..setProperties({
-      'left': 500,
-      'top': 250
-    });
+    // Cubic
 
-  var anim2 = new StyleAnimation(el)
-    ..duration = 500
-    ..setProperties({
-      'height': 250
-    });
+    test('Ease in Cubic start', () => expect(EasingEngine.easeInCubic(0, 1000, 1000, 0), 0));
+    test('Ease in Cubic middle', () => expect(EasingEngine.easeInCubic(500, 1000, 1000, 0), 125));
+    test('Ease in Cubic end', () => expect(EasingEngine.easeInCubic(1000, 1000, 1000, 0), 1000));
 
-  var queue = new AnimationQueue()
-    ..addAll([anim, anim2])
-    ..run();*/
+    test('Ease out Cubic start', () => expect(EasingEngine.easeOutCubic(0, 1000, 1000, 0), 0));
+    test('Ease out Cubic middle', () => expect(EasingEngine.easeOutCubic(500, 1000, 1000, 0), 875));
+    test('Ease out Cubic end', () => expect(EasingEngine.easeOutCubic(1000, 1000, 1000, 0), 1000));
+
+    test('Ease in/out Cubic start', () => expect(EasingEngine.easeInOutCubic(0, 1000, 1000, 0), 0));
+    test('Ease in/out Cubic middle', () => expect(EasingEngine.easeInOutCubic(500, 1000, 1000, 0), 500));
+    test('Ease in/out Cubic end', () => expect(EasingEngine.easeInOutCubic(1000, 1000, 1000, 0), 1000));
+
+    // Quartic
+
+    test('Ease in Quartic start', () => expect(EasingEngine.easeInQuartic(0, 1000, 1000, 0), 0));
+    test('Ease in Quartic middle', () => expect(EasingEngine.easeInQuartic(500, 1000, 1000, 0), 62.5));
+    test('Ease in Quartic end', () => expect(EasingEngine.easeInQuartic(1000, 1000, 1000, 0), 1000));
+
+    test('Ease out Quartic start', () => expect(EasingEngine.easeOutQuartic(0, 1000, 1000, 0), 0));
+    test('Ease out Quartic middle', () => expect(EasingEngine.easeOutQuartic(500, 1000, 1000, 0), 937.5));
+    test('Ease out Quartic end', () => expect(EasingEngine.easeOutQuartic(1000, 1000, 1000, 0), 1000));
+
+    test('Ease in/out Quartic start', () => expect(EasingEngine.easeInOutQuartic(0, 1000, 1000, 0), 0));
+    test('Ease in/out Quartic middle', () => expect(EasingEngine.easeInOutQuartic(500, 1000, 1000, 0), 500));
+    test('Ease in/out Quartic end', () => expect(EasingEngine.easeInOutQuartic(1000, 1000, 1000, 0), 1000));
+
+    // Quintic
+
+    test('Ease in Quintic start', () => expect(EasingEngine.easeInQuintic(0, 1000, 1000, 0), 0));
+    test('Ease in Quintic middle', () => expect(EasingEngine.easeInQuintic(500, 1000, 1000, 0), 31.25));
+    test('Ease in Quintic end', () => expect(EasingEngine.easeInQuintic(1000, 1000, 1000, 0), 1000));
+
+    test('Ease out Quintic start', () => expect(EasingEngine.easeOutQuintic(0, 1000, 1000, 0), 0));
+    test('Ease out Quintic middle', () => expect(EasingEngine.easeOutQuintic(500, 1000, 1000, 0), 968.75));
+    test('Ease out Quintic end', () => expect(EasingEngine.easeOutQuintic(1000, 1000, 1000, 0), 1000));
+
+    test('Ease in/out Quintic start', () => expect(EasingEngine.easeInOutQuintic(0, 1000, 1000, 0), 0));
+    test('Ease in/out Quintic middle', () => expect(EasingEngine.easeInOutQuintic(500, 1000, 1000, 0), 500));
+    test('Ease in/out Quintic end', () => expect(EasingEngine.easeInOutQuintic(1000, 1000, 1000, 0), 1000));
+
+    // Sine
+
+    test('Ease in Sine start', () => expect(EasingEngine.easeInSine(0, 1000, 1000, 0), 0));
+    test('Ease in Sine middle', () => expect(EasingEngine.easeInSine(500, 1000, 1000, 0), 292.89321881345245));
+    test('Ease in Sine end', () => expect(EasingEngine.easeInSine(1000, 1000, 1000, 0).round(), 1000));
+
+    test('Ease out Sine start', () => expect(EasingEngine.easeOutSine(0, 1000, 1000, 0), 0));
+    test('Ease out Sine middle', () => expect(EasingEngine.easeOutSine(500, 1000, 1000, 0).round(), 707));
+    test('Ease out Sine end', () => expect(EasingEngine.easeOutSine(1000, 1000, 1000, 0), 1000));
+
+    test('Ease in/out Sine start', () => expect(EasingEngine.easeInOutSine(0, 1000, 1000, 0), 0));
+    test('Ease in/out Sine middle', () => expect(EasingEngine.easeInOutSine(500, 1000, 1000, 0).round(), 500));
+    test('Ease in/out Sine end', () => expect(EasingEngine.easeInOutSine(1000, 1000, 1000, 0), 1000));
+
+    // Exp
+
+    test('Ease in Exponential start', () => expect(EasingEngine.easeInExponential(0, 1000, 1000, 0).floor(), 0));
+    test('Ease in Exponential middle', () => expect(EasingEngine.easeInExponential(500, 1000, 1000, 0), 31.25));
+    test('Ease in Exponential end', () => expect(EasingEngine.easeInExponential(1000, 1000, 1000, 0), 1000));
+
+    test('Ease out Exponential start', () => expect(EasingEngine.easeOutExponential(0, 1000, 1000, 0), 0));
+    test('Ease out Exponential middle', () => expect(EasingEngine.easeOutExponential(500, 1000, 1000, 0), 968.75));
+    test('Ease out Exponential end', () => expect(EasingEngine.easeOutExponential(1000, 1000, 1000, 0).ceil(), 1000));
+
+    test('Ease in/out Exponential start', () => expect(EasingEngine.easeInOutExponential(0, 1000, 1000, 0).floor(), 0));
+    test('Ease in/out Exponential middle', () => expect(EasingEngine.easeInOutExponential(500, 1000, 1000, 0), 500));
+    test('Ease in/out Exponential end', () => expect(EasingEngine.easeInOutExponential(1000, 1000, 1000, 0).round(), 1000));
+
+    // Circular
+
+    test('Ease in Circular start', () => expect(EasingEngine.easeInCircular(0, 1000, 1000, 0), 0));
+    test('Ease in Circular middle', () => expect(EasingEngine.easeInCircular(500, 1000, 1000, 0).round(), 134));
+    test('Ease in Circular end', () => expect(EasingEngine.easeInCircular(1000, 1000, 1000, 0), 1000));
+
+    test('Ease out Circular start', () => expect(EasingEngine.easeOutCircular(0, 1000, 1000, 0), 0));
+    test('Ease out Circular middle', () => expect(EasingEngine.easeOutCircular(500, 1000, 1000, 0).round(), 866));
+    test('Ease out Circular end', () => expect(EasingEngine.easeOutCircular(1000, 1000, 1000, 0), 1000));
+
+    test('Ease in/out Circular start', () => expect(EasingEngine.easeInOutCircular(0, 1000, 1000, 0), -500));
+    test('Ease in/out Circular middle', () => expect(EasingEngine.easeInOutCircular(500, 1000, 1000, 0), 500));
+    test('Ease in/out Circular end', () => expect(EasingEngine.easeInOutCircular(1000, 1000, 1000, 0), 1000));
+  });
+
+  group('Animations', () {
+    var el = new DivElement();
+    animate(el, duration: 1000, easing: Easing.LINEAR, properties: {'left': 200});
+
+  });
 }
